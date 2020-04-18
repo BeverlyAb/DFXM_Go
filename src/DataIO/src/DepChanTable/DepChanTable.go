e1a942922fe1d42d49bbaad87e23f05877d88583
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2020-04-18 02:48:59 -0700

    sendTo and recFrom work

--- src/DataIO/src/DepChanTable/DepChanTable.go
+++ src/DataIO/src/DepChanTable/DepChanTable.go
@@ -16,11 +16,13 @@ type DepChanTable struct{
 	DAGTable [100][100] int //rec x send
 } 
 
+//init, percentageOfCon = % of forward directed edges
 func (dct * DepChanTable)Init(size int, percent int){
 	dct.TaskSize = size
 	dct.percentageOfCon = percent
 }
 
+//Creates a pseudo-random DAG 
 func (dct * DepChanTable)CreateDAGTable(){
 	for i := 0; i < dct.TaskSize; i++ {
 	    for j := i+1; j < dct.TaskSize; j++  {
@@ -36,6 +38,32 @@ func (dct DepChanTable)CreateTaskSet(){
 
 }
 
+//creates an array of TIDs that the indexed task should send to
+func (dct DepChanTable)createSendTo(index int)[]int{
+	sendTo := make([]int,dct.TaskSize)
+	k := 0
+	for i := 0; i < dct.TaskSize; i++ {
+		if dct.DAGTable[i][index] == 1{
+			sendTo[k] = i
+			k++
+		}
+	}
+	return sendTo[0:k]
+}
+
+//creates map of TIDs that the indexed task should receive
+//data from. By default all value pair are set to false
+//since it has not received data from them
+func (dct DepChanTable)createRecFrom(index int)map[int]bool{
+	recFrom := make(map[int]bool)
+	for j := 0; j < dct.TaskSize; j++ {
+		if dct.DAGTable[index][j] == 1{
+			recFrom[j] = false
+		}
+	}
+	return recFrom
+}
+//prints DAG
 func (dct DepChanTable)PrintDAGTable(){
 	for i := 0; i < dct.TaskSize; i++ {
 	    for j := 0; j < dct.TaskSize; j++  {
@@ -52,14 +80,7 @@ func main(){
 
 	dct.Init(size,percent)
 	dct.CreateDAGTable()
-	// fmt.Println(dct.DAGTable)
 	dct.PrintDAGTable()
-	// fmt.Println(dct.DAGTable)
-	// TID_DEPTable := make(map[int][]int)
-	// TID_ChanTable := make(map[int]chan data.Data)
-	// test := DepChanTable{TID_DEPTable,TID_ChanTable}
-	// test.SET_TID_DEPTable()
-	// fmt.Println("8th's first ",test.TID_DEPTable[8][0])
-	// test.SET_TID_ChanTable()
-	// fmt.Println("Channel 5 ",test.TID_ChanTable[5])
+	fmt.Println(dct.createSendTo(1))
+	fmt.Println(dct.createRecFrom(3))
 }
--- src/DataIO/src/Task/Task.go
+++ src/DataIO/src/Task/Task.go
@@ -1,166 +1,167 @@
 package task
 
 import (
-    "fmt"
-    "github.com/klauspost/cpuid"
-    "data"
-    "math"
-    "time"
+    // "fmt"
+    // "github.com/klauspost/cpuid"
+    // "data"
+    // "math"
+    // "time"
 )
 
 type Task struct{
     TID int
-    DataDepVec[] data.Data 
-    DataOutMsg int//dataOutVec[] Data
-    PID int
-    DepCount int
-    TID_DEPTable map[int][] int
-    TID_ChanTable map[int]chan data.Data
+    Rec_from map[int]bool   //TID_rec : Received?
+    Send_to []int           //list of TID to send to
+    PID int                 //Processor ID
 } 
 
-// Find returns the smallest index i at which x == a[i],
-// or len(a) if there is no such index.
-func Find(a []data.Data, x int) int {
-    for i, n := range a {
-        if x == n.TID {
-            return i
-        }
-    }
-    return len(a)
+func (t Task)Init(TID int, recFrom map[int]bool, sendTo []int){
+    t.TID = TID
+    t.Rec_from = recFrom
+    t.Send_to = sendTo
 }
 
-func (t Task)Init(){
-    
-}
+// // Find returns the smallest index i at which x == a[i],
+// // or len(a) if there is no such index.
+// func Find(a []data.Data, x int) int {
+//     for i, n := range a {
+//         if x == n.TID {
+//             return i
+//         }
+//     }
+//     return len(a)
+// }
 
-//checks chan, if senderCountID >= recCountID, update data
-func (t Task)Receive(c <-chan data.Data){
-  //  if t.DiscernChan(c){
-        for recData := range c {
-             fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", recData.Msg)
+
+
+// //checks chan, if senderCountID >= recCountID, update data
+// func (t Task)Receive(c <-chan data.Data){
+//   //  if t.DiscernChan(c){
+//         for recData := range c {
+//              fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", recData.Msg)
                     
-            msg := recData.Msg
-            senderID := recData.TID
-            senderCountID := recData.CountID
+//             msg := recData.Msg
+//             senderID := recData.TID
+//             senderCountID := recData.CountID
 
-            i := Find(t.DataDepVec, senderID) 
-            if i != len(t.DataDepVec){
-                if senderCountID == t.DataDepVec[i].CountID{
-                    t.DataDepVec[i].Msg = msg
-                    t.DepCount++
-                    fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", t.DataDepVec[i].Msg,"Msg ID ", senderCountID)
-                    // if cpuid.CPU.LogicalCPU() % 2 ==0{
-                    //     fmt.Println("even ",cpuid.CPU.LogicalCPU())
-                    // }
-                }
-            }
-        }
-   // }
-}
+//             i := Find(t.DataDepVec, senderID) 
+//             if i != len(t.DataDepVec){
+//                 if senderCountID == t.DataDepVec[i].CountID{
+//                     t.DataDepVec[i].Msg = msg
+//                     t.DepCount++
+//                     fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", t.DataDepVec[i].Msg,"Msg ID ", senderCountID)
+//                     // if cpuid.CPU.LogicalCPU() % 2 ==0{
+//                     //     fmt.Println("even ",cpuid.CPU.LogicalCPU())
+//                     // }
+//                 }
+//             }
+//         }
+//    // }
+// }
 
-//checks if all dep. met
-func (t Task)ReadyToCompute()bool{
-    return len(t.DataDepVec) == t.DepCount
-}
+// //checks if all dep. met
+// func (t Task)ReadyToCompute()bool{
+//     return len(t.DataDepVec) == t.DepCount
+// }
 
-//computes and fire
-func (t  Task)Compute(c chan data.Data){
-    if t.ReadyToCompute(){
-        t.DepCount = 0
+// //computes and fire
+// func (t  Task)Compute(c chan data.Data){
+//     if t.ReadyToCompute(){
+//         t.DepCount = 0
 
-        var msg int = (t.TID + 1) *100
-        var TID int = t.TID
-        //var countID int = t.DataOutMsg
-        var countID = 0
-        var dataOut data.Data = data.Data{msg, TID, countID}
-        t.Fire(dataOut, c)
-        fmt.Println("Fired:\tTID ", TID, "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", dataOut.Msg, "Msg ID ", dataOut.CountID)
-    }
-}
+//         var msg int = (t.TID + 1) *100
+//         var TID int = t.TID
+//         //var countID int = t.DataOutMsg
+//         var countID = 0
+//         var dataOut data.Data = data.Data{msg, TID, countID}
+//         t.Fire(dataOut, c)
+//         fmt.Println("Fired:\tTID ", TID, "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", dataOut.Msg, "Msg ID ", dataOut.CountID)
+//     }
+// }
 
-//opens chan 
-func (t Task)Fire(dataOut data.Data, c chan data.Data){
-    defer close(c)
-    c<-dataOut
-}
+// //opens chan 
+// func (t Task)Fire(dataOut data.Data, c chan data.Data){
+//     defer close(c)
+//     c<-dataOut
+// }
 
-//returns true if task should listen on the channel, since it contains its dep
-func(t Task)DiscernChan(chanIn chan data.Data)bool{
-    var vecTID = make([]int,4)
-    vecTID = t.TID_DEPTable[t.TID]
+// //returns true if task should listen on the channel, since it contains its dep
+// func(t Task)DiscernChan(chanIn chan data.Data)bool{
+//     var vecTID = make([]int,4)
+//     vecTID = t.TID_DEPTable[t.TID]
 
-    for TID := range vecTID{
-        if t.TID_ChanTable[TID] == chanIn{
-            fmt.Println("TID ",t.TID, " SenderTID ", TID)
-            return true
-        }
-    }
-    return false
-}
+//     for TID := range vecTID{
+//         if t.TID_ChanTable[TID] == chanIn{
+//             fmt.Println("TID ",t.TID, " SenderTID ", TID)
+//             return true
+//         }
+//     }
+//     return false
+// }
 
 
-func (t * Task)Producer(iters int) <-chan data.Data {
-    c := make(chan data.Data)
-    go func() {
-        for i := 0; i < iters; i++ {
-            msg := int(math.Pow(10,float64(t.TID))) + t.DataOutMsg
-            c <- data.Data{msg,t.TID,0}
-            // fmt.Println("Fired:\tTID ", t.TID, "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", msg, "Orig Data ",t.DataOutMsg)
-            fmt.Println("TID ", t.TID, "Fired and Converted ", t.DataOutMsg, " to ",msg)
-            time.Sleep(1 * time.Second)
-        }
-        close(c)
-    }()
-    return c
-}
+// func (t * Task)Producer(iters int) <-chan data.Data {
+//     c := make(chan data.Data)
+//     go func() {
+//         for i := 0; i < iters; i++ {
+//             msg := int(math.Pow(10,float64(t.TID))) + t.DataOutMsg
+//             c <- data.Data{msg,t.TID,0}
+//             // fmt.Println("Fired:\tTID ", t.TID, "LogicalCPU ", cpuid.CPU.LogicalCPU(), "Data ", msg, "Orig Data ",t.DataOutMsg)
+//             fmt.Println("TID ", t.TID, "Fired and Converted ", t.DataOutMsg, " to ",msg)
+//             time.Sleep(1 * time.Second)
+//         }
+//         close(c)
+//     }()
+//     return c
+// }
 
-func (t * Task)Consumer(cin <-chan data.Data) {
-    for recData := range cin {
+// func (t * Task)Consumer(cin <-chan data.Data) {
+//     for recData := range cin {
             
-            msg := recData.Msg
-            senderID := recData.TID
-            senderCountID := recData.CountID
+//             msg := recData.Msg
+//             senderID := recData.TID
+//             senderCountID := recData.CountID
 
 
-            i := Find(t.DataDepVec, senderID) 
-            if i != len(t.DataDepVec){
-                if senderCountID == t.DataDepVec[i].CountID{
-                    t.DataDepVec[i].Msg = msg
-                    t.DepCount++
-                    t.DataOutMsg += msg
-                    // fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "NewData ", t.DataOutMsg,"Msg ID ", senderCountID)
-                    fmt.Println("TID ", t.TID, " Received  ",t.DataDepVec[i].Msg, " from TID ",senderID)
-                }
-            }
+//             i := Find(t.DataDepVec, senderID) 
+//             if i != len(t.DataDepVec){
+//                 if senderCountID == t.DataDepVec[i].CountID{
+//                     t.DataDepVec[i].Msg = msg
+//                     t.DepCount++
+//                     t.DataOutMsg += msg
+//                     // fmt.Println("Recvd:\tTID ", t.TID,  "LogicalCPU ", cpuid.CPU.LogicalCPU(), "NewData ", t.DataOutMsg,"Msg ID ", senderCountID)
+//                     fmt.Println("TID ", t.TID, " Received  ",t.DataDepVec[i].Msg, " from TID ",senderID)
+//                 }
+//             }
 
 
-        // fmt.Println("Msg ",recData.Msg, "SenderTID ",recData.TID, "MsgCount ",recData.CountID)
-    }
+//         // fmt.Println("Msg ",recData.Msg, "SenderTID ",recData.TID, "MsgCount ",recData.CountID)
+//     }
 
 
-}
+// }
 
-func (t Task)FanOutUnbuffered(ch <-chan data.Data, size int) []chan data.Data {
-    cs := make([]chan data.Data, size)
-    for i, _ := range cs {
-        // The size of the channels buffer controls how far behind the recievers
-        // of the fanOut channels can lag the other channels.
-        cs[i] = make(chan data.Data)
-    }
-    go func() {
-        for i := range ch {
-            for _, c := range cs {
-                c <- i
-            }
-        }
-        for _, c := range cs {
-            // close all our fanOut channels when the input channel is exhausted.
-            close(c)
-        }
-    }()
-    return cs
-}
+// func (t Task)FanOutUnbuffered(ch <-chan data.Data, size int) []chan data.Data {
+//     cs := make([]chan data.Data, size)
+//     for i, _ := range cs {
+//         // The size of the channels buffer controls how far behind the recievers
+//         // of the fanOut channels can lag the other channels.
+//         cs[i] = make(chan data.Data)
+//     }
+//     go func() {
+//         for i := range ch {
+//             for _, c := range cs {
+//                 c <- i
+//             }
+//         }
+//         for _, c := range cs {
+//             // close all our fanOut channels when the input channel is exhausted.
+//             close(c)
+//         }
+//     }()
+//     return cs
+// }
 
-func PrintTask(t Task){
-    //fmt.Println("TID ", t.TID, "CountID ",t.DataOutMsg, "LogicalCPU ",cpuid.CPU.LogicalCPU())
-}
+// func PrintTask(t Task){
+//     //fmt.Println("TID ", t.TID, "CountID ",t.DataOutMsg, "LogicalCPU ",cpuid.CPU.LogicalCPU())
+// }
