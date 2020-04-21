9ef7ba85cb2a5b5d4b9937dfb989e8758cbdb12d
Beverly Abadines <BeverlyAb@users.noreply.github.com>
2020-04-21 13:10:43 -0700

    Append did not work. Iterate through until all runset = -1

--- src/DataIO/DataIO.go
+++ src/DataIO/DataIO.go
@@ -4,6 +4,7 @@ package main
 //https://blog.golang.org/pipelines (multi-listener chan)
 // https://flaviocopes.com/golang-event-listeners/
 ///https://play.golang.org/p/I-GA6DYd0HK //Fanout
+//https://golangbot.com/arrays-and-slices/ //slices
 import (
     // "github.com/klauspost/cpuid"
     "time"
@@ -27,12 +28,13 @@ func main(){
     dct.CreateTaskSet()
     fmt.Println(dct.TaskSet[0:dct.TaskSize])
     
-    runset := createRunSet(size)[:] //slice
+    runset := createRunSet(size) //slice
 
-    for j := 0; j < 3; j++ {           
+    for ;keepRunning(runset);{    
         for i := 0; i < size; i++{
-            if dct.TaskSet[i].Fire(&dct.TaskSet){
+            if dct.TaskSet[i].Fire(&dct.TaskSet) && isInRunSet(runset,i){
                 updateRunSet(runset,i)
+                fmt.Println("TID ",i,"Fired")
             }
         }
     }           
@@ -55,10 +57,27 @@ func main(){
 //removes task that has ran
 //why * isn't needed?
 func updateRunSet(runset  []int, index int){
-    // runset[index] = runset[len(runset)-1]
-    runset[len(runset)-1] = 0
-    // runset = runset[:len(runset)-1]
-    runset = append(runset[:index],runset[index+1:]...)
+    runset[index] = -1
+    //runset = append(runset[:index],runset[index+1:]...)
     fmt.Println(runset)
+}
+
+func isInRunSet(runset[] int,tid int)bool{
+    for _,elem := range runset{
+        if elem == tid {
+            return true
+        }
+    }
+    return false
+}
+
+func keepRunning(runset []int)bool{
+    for _,tid := range runset{
+        if tid != -1{
+            fmt.Println("TID : ",tid, "Still needs to run")
+            return true
+        }
+    }
+    return false
 }
 
